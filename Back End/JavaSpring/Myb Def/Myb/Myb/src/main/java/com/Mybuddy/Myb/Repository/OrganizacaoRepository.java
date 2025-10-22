7d9e61309ee7ed3913ebf02781070d7ff34fb14f
Julia Cardoso <juliacardoso8080@gmail.com>
2025-10-22 15:35:07 -0300

    Ajuste Repository

--- Back End/JavaSpring/Myb Def/Myb/Myb/src/main/java/com/Mybuddy/Myb/Repository/OrganizacaoRepository.java
+++ Back End/JavaSpring/Myb Def/Myb/Myb/src/main/java/com/Mybuddy/Myb/Repository/OrganizacaoRepository.java
@@ -1,5 +1,10 @@
 package com.Mybuddy.Myb.Repository;
 
-public interface OrganizacaoRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import com.Mybuddy.Myb.Model.Organizacao;
 
+@Repository
+public interface OrganizacaoRepository extends JpaRepository<Organizacao, Long> {
+   
 }
