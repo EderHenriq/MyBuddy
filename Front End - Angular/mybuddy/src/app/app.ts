8573cd4584133004cc815996f7955e7bca437d86
Godinho <danielgodinho772@gmail.com>
2026-03-15 20:31:57 -0300

    feat: [MY-150] Configurar Angular Material ou PrimeNG como UI library

--- Front End - Angular/mybuddy/src/app/app.ts
+++ Front End - Angular/mybuddy/src/app/app.ts
@@ -1,12 +1,16 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { ButtonModule } from 'primeng/button';
+import { TooltipModule } from 'primeng/tooltip';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [ButtonModule, TooltipModule],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
 export class App {
   protected readonly title = signal('mybuddy');
 }
+
+export class AppComponent extends App {}
