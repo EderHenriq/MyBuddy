a65f9b1621dc417dba9750e6820445f3d9b5ce78
Seu Nome <seu-email@exemplo.com>
2026-03-15 15:54:41 -0300

    Refatorando classes de request  com lombok

--- Back End/JavaSpring/Myb Def/Myb/Myb/src/main/java/com/Mybuddy/Myb/Payload/Request/LoginRequest.java
+++ Back End/JavaSpring/Myb Def/Myb/Myb/src/main/java/com/Mybuddy/Myb/Payload/Request/LoginRequest.java
@@ -1,29 +1,16 @@
 package com.Mybuddy.Myb.Payload.Request;
 
 import jakarta.validation.constraints.NotBlank;
+import lombok.Getter;
+import lombok.Setter;
 
+@Getter
+@Setter
 public class LoginRequest {
 
     @NotBlank
-    private String email; // Campo para o e-mail do usuário
+    private String email;
 
-    @NotBlank // NOVO: Este campo é para a senha real do usuário
-    private String password; // Campo para armazenar a senha do usuário
-
-    // --- Getters e Setters ---
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getPassword() { // Getter para a senha
-        return password;
-    }
-
-    public void setPassword(String password) { // Setter para a senha
-        this.password = password;
-    }
+    @NotBlank
+    private String password;
 }
