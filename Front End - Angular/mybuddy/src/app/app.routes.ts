7f60f763e67870bd236e55b1eb809dd1c33bcba3
Godinho <danielgodinho772@gmail.com>
2026-03-16 03:01:22 -0300

    feat: [MY-157] Criar estrutura de roteamento principal (app-routing.module)

--- Front End - Angular/mybuddy/src/app/app.routes.ts
+++ Front End - Angular/mybuddy/src/app/app.routes.ts
@@ -2,19 +2,22 @@ import { Routes } from '@angular/router';
 
 export const routes: Routes = [
   {
+    // Redirecionamento inicial para a rota 'home'
     path: '',
-    redirectTo: 'home',
-    pathMatch: 'full',
+    loadComponent: () => import('./features/landing-page/landing-page').then(m => m.LandingPage),
   },
   {
+    // Rota para a página inicial (home)
     path: 'home',
     loadComponent: () => import('./features/home/home').then(m => m.Home),
   },
   {
+    // Rota para a página de pets
     path: 'pets',
     loadComponent: () => import('./features/pets/pets').then(m => m.Pets),
   },
   {
+    // Rota para páginas não encontradas (404)
     path: '**',
     loadComponent: () => import('./shared/components/not-found/not-found').then(m => m.NotFound),
   },
