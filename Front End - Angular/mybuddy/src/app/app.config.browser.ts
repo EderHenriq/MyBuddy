3f888b93e2d1e82c91784ec9ed1c2fa6a6db93e0
A-DAVI <cassolidavi603@gmail.com>
2026-04-25 14:38:11 -0300

    fix(frontend): remove provideHttpClient duplicado do app.config.ts  [MY-128] #done

--- Front End - Angular/mybuddy/src/app/app.config.browser.ts
+++ Front End - Angular/mybuddy/src/app/app.config.browser.ts
@@ -1,12 +1,24 @@
 import { ApplicationConfig, mergeApplicationConfig } from '@angular/core';
 import { environment } from '../environments/environment';
 import { appConfig } from './app.config';
-import { provideKeycloak } from 'keycloak-angular';
+import { authInterceptor } from './core/interceptors/auth.interceptor';
+import { provideHttpClient, withFetch, withInterceptors } from '@angular/common/http';
+import { provideKeycloak, INCLUDE_BEARER_TOKEN_INTERCEPTOR_CONFIG } from 'keycloak-angular';
 
 const browserConfig: ApplicationConfig = {
     providers: [
+        provideHttpClient(withFetch(), withInterceptors([authInterceptor])),
+        {
+            provide: INCLUDE_BEARER_TOKEN_INTERCEPTOR_CONFIG,
+            useValue: [
+                {
+                    urlPattern: /^(http:\/\/localhost:8081)(\/.*)?$/,
+                    bearerPrefix: "Bearer "
+                },
+            ]
+        },
         provideKeycloak({
-            config:{
+            config: {
                 url: environment.keycloak.url,
                 realm: environment.keycloak.realm,
                 clientId: environment.keycloak.clientId,
