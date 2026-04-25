3cd331304bc14d623758f509f5545ade6dc50482
A-DAVI <cassolidavi603@gmail.com>
2026-04-25 12:20:52 -0300

    fix(frontend): ajustando mencao ao keycloak

--- Front End - Angular/mybuddy/src/environments/environment.development.ts
+++ Front End - Angular/mybuddy/src/environments/environment.development.ts
@@ -2,7 +2,7 @@ export const environment = {
     production: false,
     apiUrl: 'https://localhost:8081/api/',
     envName: 'Development',
-    Keycloak: {
+    keycloak: {
         url: 'https://localhost:8080',
         realm: 'mybuddy',
         clientId: 'mybuddy-frontend',
