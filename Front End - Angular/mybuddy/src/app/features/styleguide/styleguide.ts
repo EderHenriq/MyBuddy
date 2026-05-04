49cdb4eff59a5d1502ebe3ab807b6a188d3ce086
Godinho <danielgodinho772@gmail.com>
2026-05-03 22:06:53 -0300

    feat: [MY-230] criado componente do style-guide, e adicionado html que explicam o design system

---
+++ Front End - Angular/mybuddy/src/app/features/styleguide/styleguide.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { InputText } from 'primeng/inputtext';
+import { Checkbox } from 'primeng/checkbox';
+import { Button } from 'primeng/button';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-styleguide',
+  imports: [InputText, Checkbox, Button, FormsModule],
+  templateUrl: './styleguide.html',
+  styleUrl: './styleguide.scss',
+})
+export class Styleguide {}
