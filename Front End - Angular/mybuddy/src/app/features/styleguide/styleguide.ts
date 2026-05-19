91c82110f1a09ee8e9bd34b9fa090afb0d3bb641
Godinho <danielgodinho772@gmail.com>
2026-05-19 15:23:58 -0300

    refactor: [MY-13] ajustes no error-page

--- Front End - Angular/mybuddy/src/app/features/styleguide/styleguide.ts
+++ Front End - Angular/mybuddy/src/app/features/styleguide/styleguide.ts
@@ -3,11 +3,12 @@ import { InputText } from 'primeng/inputtext';
 import { Checkbox } from 'primeng/checkbox';
 import { Button } from 'primeng/button';
 import { FormsModule } from '@angular/forms';
+import { ErrorPage } from '../../shared/components/error-page/error-page';
 import { EmptyState } from '../../shared/components/empty-state/empty-state';
 
 @Component({
   selector: 'app-styleguide',
-  imports: [InputText, Checkbox, Button, FormsModule, EmptyState],
+  imports: [InputText, Checkbox, Button, FormsModule, ErrorPage, EmptyState],
   templateUrl: './styleguide.html',
   styleUrl: './styleguide.scss',
 })
--- Front End - Angular/mybuddy/src/app/shared/components/error-page/error-page.ts
+++ Front End - Angular/mybuddy/src/app/shared/components/error-page/error-page.ts
@@ -1,18 +1,17 @@
 import { Component, Input, Output, EventEmitter } from '@angular/core';
-import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-error-page',
   standalone: true,
-  imports: [CommonModule],
+  imports: [],
   templateUrl: './error-page.html',
-  styleUrls: ['./error-page.scss']
+  styleUrls: ['./error-page.scss'],
 })
 export class ErrorPage {
   @Input() errorCode?: string; // Ex: '404', '500'
-  @Input() title: string = 'Ops! Algo deu errado.';
-  @Input() description: string = 'Não conseguimos carregar esta página. Verifique sua conexão ou tente novamente mais tarde.';
-  @Input() actionLabel: string = 'Voltar para o início';
+  @Input() title = 'Ops! Algo deu errado.';
+  @Input() description = 'Não conseguimos carregar esta página. Verifique sua conexão ou tente novamente mais tarde.';
+  @Input() actionLabel = 'Voltar para o início';
   @Output() actionClick = new EventEmitter<void>();
 
   onActionClick() {
