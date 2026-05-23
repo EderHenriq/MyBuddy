0b00360e203a99044dbeb3a1e4274df5da061913
user.email <cassolidavi603@gmail.com>
2026-05-23 15:33:42 -0300

    fix: MY-232: adicionar fallback de leitura do token direto do cookie no authInterceptor

--- Front End - Angular/mybuddy/src/app/core/interceptors/auth.interceptor.ts
+++ Front End - Angular/mybuddy/src/app/core/interceptors/auth.interceptor.ts
@@ -4,7 +4,13 @@ import { AuthService } from '../services/auth.service';
 
 export const authInterceptor: HttpInterceptorFn = (req, next) => {
     const authService = inject(AuthService);
-    const token = authService.getToken();
+    
+    let token = authService.getToken();
+
+    if (!token) {
+        const matches = document.cookie.match(/(?:^|; )mybuddy_session=([^;]*)/);
+        token = matches ? decodeURIComponent(matches[1]) : undefined;
+    }
 
     if (token) {
         const authReq = req.clone({
