2b6cae6cbc8eb10995eabaf1212f8b9ca9fbf601
Godinho <danielgodinho772@gmail.com>
2026-05-24 15:45:29 -0300

    refactor: [MY-353] ajustes no componente de card, adicionado codigo do card-pets para reduzir redundancias

--- Front End - Angular/mybuddy/src/app/shared/components/cards/cards.ts
+++ Front End - Angular/mybuddy/src/app/shared/components/cards/cards.ts
@@ -1,12 +1,37 @@
 import { Pet } from '../../../core/models/pet.model';
-import { Component, Input } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { CommonModule, NgClass } from '../../../../../node_modules/@angular/common/types/_common_module-chunk';
 
 @Component({
   selector: 'app-cards',
-  imports: [],
+  standalone: true,
+  imports: [NgClass, CommonModule],
   templateUrl: './cards.html',
   styleUrl: './cards.scss',
 })
 export class Cards {
   @Input() pet?: Pet;
+
+  @Input() ongMode: boolean;
+  @Input() showFavoriteButton: boolean;
+  @Input() showTopHeart: boolean;
+  @Input() isFavorite: boolean;
+
+  @Input() badgeText?: string;
+  @Input() badgeType: string;
+
+  @Output() editClick = new EventEmitter<Event>();
+  @Output() deleteClick = new EventEmitter<Event>();
+  @Output() favoriteClick = new EventEmitter<Event>();
+
+  isModalVisible: boolean;
+
+  openModal() {
+    this.isModalVisible = true;
+  }
+
+  handleAdopt() {
+    console.log('Solicitação de adoção para: ${this.pet?.name}');
+    this.isModalVisible = false;
+  }
 }
