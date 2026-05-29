d245aefaacab48619d73869e1717ed717fd27150
user.email <cassolidavi603@gmail.com>
2026-05-28 22:45:27 -0300

    feat: MY-318: Configurar BLoC como gerenciador de estado com AuthBloc, events e states

---
+++ mobile/lib/features/auth/domain/repositories/auth_repository.dart
@@ -0,0 +1,10 @@
+import 'package:dartz/dartz.dart';
+import 'package:mybuddy_app/core/errors/failures.dart';
+import 'package:mybuddy_app/features/auth/domain/entities/user.dart';
+
+abstract class AuthRepository {
+  Future<Either<Failure, User>> login(String email, String password);
+  Future<Either<Failure, void>> logout();
+  Future<Either<Failure, User>> getProfile();
+  Future<bool> isAuthenticated();
+}
