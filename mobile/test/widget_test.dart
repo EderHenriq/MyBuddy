57bb4088278ec1fe183dbeefdac812023fef0dc1
Eder Henrique P. <eder.pontes2014@gmail.com>
2026-06-09 01:42:41 -0300

    test(mobile): MY-336 add test verifying light and dark theme configurations

--- mobile/test/widget_test.dart
+++ mobile/test/widget_test.dart
@@ -1,16 +1,17 @@
+import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mybuddy_app/app.dart';
 import 'package:mybuddy_app/core/constants/app_config.dart';
 import 'package:mybuddy_app/core/di/injection_container.dart' as di;
 
 void main() {
-  setUp(() async {
+  setUpAll(() async {
     // Inicializa a injeção de dependência e flavors para os testes
     AppConfig.initialize(flavorType: Flavor.dev);
     await di.init();
   });
 
-  tearDown(() async {
+  tearDownAll(() async {
     await di.sl.reset();
   });
 
@@ -22,6 +23,17 @@ void main() {
     expect(find.byType(MyBuddyApp), findsOneWidget);
 
     // Aguarda o timer da SplashPage terminar para não deixar timers pendentes
+    await tester.pump(const Duration(seconds: 3));
+  });
+
+  testWidgets('Verifica configuracao de temas claro e escuro no MaterialApp', (WidgetTester tester) async {
+    await tester.pumpWidget(const MyBuddyApp());
+    final MaterialApp materialApp = tester.widget(find.byType(MaterialApp));
+    
+    expect(materialApp.theme?.brightness, Brightness.light);
+    expect(materialApp.darkTheme?.brightness, Brightness.dark);
+    expect(materialApp.themeMode, ThemeMode.system);
+    
     await tester.pump(const Duration(seconds: 3));
   });
 }
